f53bc0d85bcd5277738aaefa8bb668f2da99fbd7
rodas94 <rodas@rodas.com>
2026-08-16 16:49:49 +0300

    fix: resolve exam portal authentication bug

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 // Protects routes requiring admin login
 exports.requireAuth = (req, res, next) => {
+  // If the user is logged in, proceed to the next middleware or route handler
   if (req.session && req.session.user) {
     return next();
   }
